4670187437a4db839add92f8954e737fc2f18679
Charles Francoise <charles.francoise@circleci.com>
2026-01-15 11:30:59 +0100

    setup acceptance test
    BREAKING CHANGE: update eslint-plugin-react-hooks major version

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,26 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
 import baseConfig from './src/base/config.js';
 import formattingConfig from './src/formatting/config.js';
 import importConfig from './src/import/config.js';
 import jsdocConfig from './src/jsdoc/config.js';
+import vitestConfig from './src/vitest/config.js';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  { ignores: ['node_modules'] },
+export default defineConfig([
+  { ignores: ['node_modules', 'test/acceptance/fixtures'] },
   ...baseConfig,
   ...importConfig,
   ...jsdocConfig,
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    extends: [vitestConfig],
+  },
   ...formattingConfig, // Must be last
-];
+]);
